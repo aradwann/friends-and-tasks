645cec762b5687897bfd49546ee9417286842ca6
Ahmed Radwan <aradwann@proton.me>
2022-08-04 16:04:11 +0200

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //////////////////// SETUP VALIDATION PIPE ///////////////
   app.useGlobalPipes(
     new ValidationPipe({
       // setting whitelist to true trim any additional unwanted props/values in the request body
@@ -19,6 +22,17 @@ async function bootstrap() {
       },
     }),
   );
+
+  ////////////////////////////// SETUP SWAGGER (OPEN API) ////////////////////////
+  const options = new DocumentBuilder()
+    .setTitle('Friends and Tasks')
+    .setDescription('Users who are friend and working on tasks together')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,9 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -27,16 +29,19 @@ export class UsersController {
     return this.usersService.findAll(paginationQuery);
   }
 
+  @ApiNotFoundResponse({ description: 'not found' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
+  @ApiNotFoundResponse({ description: 'not found' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @ApiNotFoundResponse({ description: 'not found' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
