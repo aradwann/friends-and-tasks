a426fb783caafd09c10f623929a752b7f5ee7b84
Ahmed Radwan <aradwann@proton.me>
2022-08-04 13:40:31 +0200

    feat: enable validation pipe implicit conversion

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: {
+        // enable implicit conversion of the received params over the network
+        // as the network transfer all values as strings we need to convert them
+        // to their correct types to be appropriatly used
+        // such as query params used in pagination
+        // enabling this option makes the params converted to integers automatically
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
