104dbdfd9f180f41d4a857a1f6e2a29d1ffbcf01
Ahmed Radwan <aradwann@proton.me>
2022-08-21 18:47:11 +0200

    initiate workspace resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { WorkspacesModule } from './workspaces/workspaces.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -35,6 +36,7 @@ import * as Joi from 'joi';
     TasksModule,
     UsersModule,
     AuthModule,
+    WorkspacesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,4 @@
 import { IsArray, IsString } from 'class-validator';
-import { User } from 'src/users/entities/user.entity';
-import { Status } from '../enums/status.enum';
 
 export class CreateTaskDto {
   @IsString()
@@ -8,9 +6,6 @@ export class CreateTaskDto {
 
   @IsString()
   description: string;
-
-  @IsString()
-  status: Status;
 
   @IsArray()
   assignees: number[]; // array of users IDs
