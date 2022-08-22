b3e99d7491bea9a2fd98f4804e7148a41f20f4dd
Ahmed Radwan <aradwann@proton.me>
2022-08-22 14:40:21 +0200

    implement public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from '../users/decorators/user.decorator';
+import { User } from '../users/entities/user.entity';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
+import { LoginDto } from './dto/login.dto';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @ApiTags('auth')
@@ -8,10 +12,11 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard) // this decorator envokes (glues up) the local strategy code to authenticate the user credentials
   @Post('login')
   @HttpCode(200)
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(@Body() _loginDto: LoginDto, @CurrentUser() user: User) {
+    return this.authService.login(user);
   }
 }
