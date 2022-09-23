5d0734aeed9633c98dca104f0921ff52d46d9694
Ahmed Radwan <aradwann@proton.me>
2022-09-23 19:19:18 +0200

    remove unecessary eager loading from task entity

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -35,20 +35,16 @@ export class Task {
   status: Status;
 
   @ManyToOne(() => User, (user) => user.tasks_posted, {
-    eager: true,
     onDelete: 'CASCADE', // if the assignor of the task is deleted, then the task is deleted
   })
   assignor: User;
 
   @ManyToOne(() => Workspace, (workspace) => workspace.tasks, {
-    eager: true,
     onDelete: 'CASCADE', // if the workspace of the task is deleted, then the task is deleted
   })
   workspace: Workspace;
 
-  @ManyToMany(() => User, (user) => user.tasks_assigned_to_user, {
-    eager: true,
-  })
+  @ManyToMany(() => User, (user) => user.tasks_assigned_to_user, {})
   @JoinTable()
   assignees: User[];
 }
